2fae84f8ac507b9c242aa673c2c4f0111682d4f5
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-02-19 15:52:22 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,19 @@ st.set_page_config(page_title="Budgetting App", page_icon='tada', layout='wide')
 
 st.header("Budgetting App For Apartment")
 
-rent1 = int(st.input("Monthly Rent Cost"))
+rent1 = (st.input("Monthly Rent Cost")
 
-gas1 = int(st.input("Monthly Gas Cost: "))
+gas1 = (st.input("Monthly Gas Cost: ")
 
-food1 = int(st.input("Monthly Food Cost: "))
+food1 = (st.input("Monthly Food Cost: ")
 
-girl1 = int(st.input("Monthly Girl Cost (If you get no bitches, just throw in a 0): "))
+girl1 = (st.input("Monthly Girl Cost (If you get no bitches, just throw in a 0): ")
 
-gambling1 = int(st.input("Monthly Gambling Cost dont cap: "))
+gambling1 = (st.input("Monthly Gambling Cost dont cap: ")
 
-substances1 = int(st.input("Monthly Alchohol, Weed, and Nic costs: "))
+substances1 = (st.input("Monthly Alchohol, Weed, and Nic costs: ")
 
-personal1 = int(st.input("Monthly Personal Spending Cost: "))
+personal1 = (st.input("Monthly Personal Spending Cost: ")
 
 total = (rent1 + gas1 + food1 + girl1 + gambling1 + substances1 + personal1)
 st.header("Total price = " + total)
