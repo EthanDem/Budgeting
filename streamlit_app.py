4c63052f4743685453e5d4883f143e84b05a6dcc
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-02-19 16:47:12 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ girl1 = st.number_input("Monthly Girl Cost (If you get no bitches, just throw in
 
 gambling1 = st.number_input("Monthly Gambling Cost dont cap: ", step = 1)
 
-substances1 = st.number_input("Monthly Alchohol, Weed, and Nic costs: ", step = 1)
+substances1 = st.number_input("Monthly Alchohol And Substances Cost: ", step = 1)
 
 personal1 = st.number_input("Monthly Personal Spending Cost: ", step = 1)
 
@@ -22,7 +22,7 @@ total = (rent1 + gas1 + food1 + girl1 + gambling1 + substances1 + personal1)
 
 if st.button("Submit Calculations"):
   st.header("Your total monthly cost is:")
-  st.header(total)
+  st.write(total)
 
 
 
